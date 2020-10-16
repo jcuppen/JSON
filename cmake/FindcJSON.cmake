699f1bffec285c2947554d7e7fad87846f26b0f6
Job Cuppen <job@Jobs-MacBook-Pro.local>
2020-10-16 13:02:56 +0200

    [WIP] added more cmake files to find cJSON

---
+++ cmake/FindcJSON.cmake
@@ -0,0 +1,14 @@
+# CMake module to search for the cJSON library
+# (library for printing JSON files)
+#
+# If it's found it sets CJSON_FOUND to TRUE
+# and following variables are set:
+#    CJSON_INCLUDE_DIR
+#    CJSON_LIBRARIES
+
+FIND_PATH(CJSON_INCLUDE_DIR NAMES cjson.h)
+FIND_LIBRARY(CJSON_LIBRARIES NAMES json cjson)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(cJSON DEFAULT_MSG CJSON_LIBRARIES CJSON_INCLUDE_DIR)
+MARK_AS_ADVANCED(CJSON_INCLUDE_DIR CJSON_LIBRARIES)
