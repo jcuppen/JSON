3d15893d1b6934f05953e8084c1e2ee804eacd73
Job Cuppen <job@Jobs-MacBook-Pro.local>
2020-10-29 13:53:11 +0100

    Added: - setBool function allows adding a boolean to the specified json object - setTrue function allows adding a true to the specified json object - setFalse function allows adding a false to the specified json object Modified: - Updated example files to demonstrate new functionality - Updated example/readme to reflect changes - Updated readme to reflect changes

--- examples/README.md
+++ examples/README.md
@@ -5,10 +5,23 @@ sac2c FILENAME.sac
 ./a.out
 ```
 ## Files
-### `printing_json.sac`
+### `printing_json_empty.sac`
 Creates an empty json object and outputs it to stdout.
 expected result:
-```bash
+```json
 {
+}
+```
+
+### `printing_json_booleans.sac`
+Creates a json object and sets some booleans in various ways.
+Then outputs the json object to stdout.
+expected result:
+```json
+{
+	"isTrue":		true,
+	"isFalse":		false,
+	"isAlsoTrue":	true,
+	"isAlsoFalse":	false
 }
 ```
--- src/src/JSONBuilder/json_builder.c
+++ src/src/JSONBuilder/json_builder.c
@@ -1,9 +1,27 @@
 #include <cjson/cJSON.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 cJSON * create_object()
 {
 	return cJSON_CreateObject();
+}
+
+void set_bool( cJSON * object, char * key, bool boolean)
+{
+	cJSON * val;
+	val = boolean ? cJSON_CreateTrue() : cJSON_CreateFalse();
+	cJSON_AddItemToObject(object, key, val);
+}
+
+void set_true( cJSON * object, char * key) 
+{
+	set_bool(object, key, true);
+}
+
+void set_false( cJSON * object, char * key) 
+{
+	set_bool(object, key, false);
 }
 
 void serialize_object( cJSON* object)
